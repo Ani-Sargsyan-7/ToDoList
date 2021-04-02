a561efeca74404212eccd3f4fdf01b9c2f1078df
ani777-77/react_das <anisargsyan777@gmail.com>
2021-04-02 07:43:07 -0700

    changes

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -3,28 +3,32 @@ import {Link} from 'react-router-dom';
 import {Container, Row, Col} from 'react-bootstrap';
 import {FaAngleDoubleRight} from 'react-icons/fa';
 
-import Capture  from '../../../images/Capture.png'
+import Captures  from '../../../images/Captures.png'
 import styles from './about.module.css';
 
 export default function About(){
     return(
        <Container>
-       <Row className='justify-content-start'>
-       <Col xs={11}>
+       <Row className='justify-content-center'>
        <div className = {styles.about}>
-            <h2>This is an application for writing your daily tasks.</h2>
-            <h3>If you want to be our user you can register there...</h3>
+       <Col xs={12}>
+
+            <h2 className={styles.text}>This is an application for writing your daily tasks.</h2>
+            <h3 className={styles.text}>If you want to be our user you can register there...</h3>
             <Link
             className={styles.link}
             to='/register'
             >
             Sign Up here <FaAngleDoubleRight/>
          </Link>
-         <div className = {styles.img}>
-            <img src = {Capture} alt="Tasks Page"/>
-        </div>
-       </div>
        </Col>
+
+         <Col xs={10}>
+         <div className = {styles.img}>
+            <img src = {Captures} alt="Tasks Page"/>
+        </div>
+        </Col>
+       </div>
        </Row>
        </Container>
     );
